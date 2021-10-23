744b27ad9d9a98c2ab1f780f84c08a27181089e2
mis <mearlboro@protonmail.com>
2021-10-23 17:07:42 +0100

    Add multiple network sizes to retweet graph viz

--- assets/js/network.js
+++ assets/js/network.js
@@ -19,13 +19,29 @@ var GetCol = function(group) {
     }
 }
 
+var GetNetwork = function(size, live) {
+    if (live) {
+        if (size == 150)  data = retweets_150;
+        if (size == 500)  data = retweets_500;
+        if (size == 1500) data = retweets_1500;
+    }
+    else {
+        if (size == 150)  data = retweets_150;
+        if (size == 500)  data = retweets_500;
+        if (size == 1500) data = retweets_1500;
+    }
+    return data;
+}
+
 var GetTwitterLink = function(handle) {
     return '<a href="https://twitter.com/' + handle.slice(1) +
         '" target="_blank">' + handle + '</a>';
 }
 
-var GetMostRetweeted = function(handle) {
-    var sorted_to = graph_data['links']
+var GetMostRetweeted = function(handle, size, live) {
+    data = GetNetwork(size, live);
+
+    var sorted_to = data['links']
         .filter(l => l.source['name'] === handle)
         .sort((l1, l2) => l2['value'] - l1['value']);
     if (sorted_to.length > 3) {
@@ -35,7 +51,7 @@ var GetMostRetweeted = function(handle) {
         return { 'name': l.target['name'], 'val': l.value }
     });
 
-    var sorted_from = graph_data['links']
+    var sorted_from = data['links']
         .filter(l => l.target['name'] === handle)
         .sort((l1, l2) => l2['value'] - l1['value']);
     if (sorted_from.length > 3) {
@@ -52,7 +68,7 @@ var ClosePanel = function() {
     document.getElementById('panel').classList.add('hide');
 }
 
-var OpenPanel = function(handle) {
+var OpenPanel = function(handle, size, live) {
     var user = handle.slice(1)
     document.getElementById('panel_title').innerHTML = handle;
 
@@ -73,7 +89,7 @@ var OpenPanel = function(handle) {
 
     // add most retweeted by user and who retweeted user most
     panel.innerHTML += '<p>Top users retweeted by this account:<br/>';
-    links = GetMostRetweeted(handle);
+    links = GetMostRetweeted(handle, size, live);
     links[0].forEach(l => panel.innerHTML += GetTwitterLink(l['name']) + ' (' + l['val'] + ' retweets)<br/>');
     panel.innerHTML += '</p><p>Top users who retweeted this account:<br/>';
     links[1].forEach(l => panel.innerHTML += GetTwitterLink(l['name']) + ' (' + l['val'] + ' retweets)<br/>');
@@ -91,43 +107,51 @@ var GetHelp = function() {
     document.getElementById('panel').classList.remove('hide');
 }
 
-const Graph = ForceGraph()(
-  document.getElementById("graph"))
-    .graphData(graph_data)
-    .backgroundColor("#ffffff")
-    .zoom(0.6)
-    .width(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - 100)
-    .height(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 140)
-    // configure nodes
-    .nodeColor(n => GetCol(n.group))
-    .nodeVal(n => n.value * 4000.0)
-    // configure links, particles travelling links indicate link direction
-    .linkCurvature(.2)
-    .linkDirectionalParticles(l => l.value)
-    .linkDirectionalParticleSpeed(l => l.value * 0.001)
-    .linkDirectionalParticleWidth(3)
-    // decorate the node
-    .nodeCanvasObject((n, ctx, globalScale) => {
-      const label = n.name;
-      ctx.font =  (Math.pow(2, 1 + n.value * 20) * 10).toString() + 'px monospace';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.strokeStyle = '#ffffff';
-      ctx.lineWidth = 4;
-      ctx.strokeText(label, n.x, n.y + 12);
-      ctx.fillStyle = n.color;
-      ctx.fillText(label, n.x, n.y + 12);
-    })
-    .nodeCanvasObjectMode(() => 'after')
-    // node remains in position after dragging
-    .onNodeDragEnd(node => {
-      node.fx = node.x;
-      node.fy = node.y;
-    })
-    // show panel on node click
-    .onNodeClick(n => OpenPanel(n.name))
+var LoadNetwork = function(size, live) {
+    data = GetNetwork(size, live);
 
-// Spread nodes a little wider
-Graph.d3Force("charge").strength(-900);
-Graph.d3VelocityDecay(0.1)
+    const Graph = ForceGraph()(
+      document.getElementById("graph"))
+        .graphData(data)
+        .backgroundColor("#ffffff")
+        .zoom(0.6)
+        .width(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - 100)
+        .height(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 140)
+        // configure nodes
+        .nodeColor(n => GetCol(n.group))
+        .nodeVal(n => n.value * 4000.0)
+        // configure links, particles travelling links indicate link direction
+        .linkCurvature(.2)
+        .linkDirectionalParticles(l => l.value)
+        .linkDirectionalParticleSpeed(l => l.value * 0.001)
+        .linkDirectionalParticleWidth(3)
+        // decorate the node
+        .nodeCanvasObject((n, ctx, globalScale) => {
+          const label = n.name;
+          ctx.font =  (Math.pow(2, 1 + n.value * 20) * 10).toString() + 'px monospace';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.strokeStyle = '#ffffff';
+          ctx.lineWidth = 4;
+          ctx.strokeText(label, n.x, n.y + 12);
+          ctx.fillStyle = n.color;
+          ctx.fillText(label, n.x, n.y + 12);
+        })
+        .nodeCanvasObjectMode(() => 'after')
+        // node remains in position after dragging
+        .onNodeDragEnd(node => {
+          node.fx = node.x;
+          node.fy = node.y;
+        })
+        // show panel on node click
+        .onNodeClick(n => OpenPanel(n.name, size, live))
 
+    // Spread nodes a little wider
+    Graph.d3Force("charge").strength(-900);
+    Graph.d3VelocityDecay(0.1)
+
+    ClosePanel();
+
+}
+
+LoadNetwork(150, false);
--- index.md
+++ index.md
@@ -5,12 +5,23 @@ title: COP26 Retweet Network
 head_js:
  - "https://unpkg.com/force-graph"
 body_js:
- - "/assets/data/retweets.js"
+ - "/assets/data/retweets_150.js"
+ - "/assets/data/retweets_500.js"
+ - "/assets/data/retweets_1500.js"
  - "/assets/js/network.js"
  - "https://platform.twitter.com/widgets.js"
 ---
 
-<p class="small">(<a onclick="GetHelp()">About this visualisation</a>)</p>
+<a href="/visualisation/communities">See the communities in this network</a>
+
+<div class="controls">
+<input type="checkbox"><label for="live" class="small">Live data</label><br/>
+<br/>
+<p class="small">Network size:</p>
+<input name="size" value="150"  onclick="LoadNetwork(150 , false)"  type="radio" checked><label for="150" >150</label><br/>
+<input name="size" value="500"  onclick="LoadNetwork(500 , false)"  type="radio"><label for="500" >500</label><br/>
+<input name="size" value="1500" onclick="LoadNetwork(1500, false)" type="radio"><label for="1500">1500</label><br/>
+</div>
 
 <div id="graph">
 </div>
@@ -20,3 +31,6 @@ body_js:
   <h3 id='panel_title'></h3>
   <div id='panel_content'></div>
 </div>
+
+<p class="small">(<a onclick="GetHelp()">About this visualisation</a>)</p>
+
