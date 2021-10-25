63bed0570c66fe7ba340d1c4b1e935d70a28b5b2
mis <mearlboro@protonmail.com>
2021-10-26 00:19:42 +0100

    Add ego networks to communities page

--- assets/js/communities.js
+++ assets/js/communities.js
@@ -1,9 +1,123 @@
-Draw = (function() {
-  var svg = d3.select("#circles");
+"use strict";
 
+var GetData = (function(username, level) {
+    var root = graph_data['nodes'].filter(
+      n => n.name.toLowerCase() === username.toLowerCase())[0];
+    if (!root) return 0;
+
+    var id = root.id
+
+    // get all links containing this node and their node ids
+    var dir_links = graph_data['links'].filter(
+        l => l['source'] === id || l['target'] == id);
+    var dir_ids = dir_links.map(
+      l => l['source'] === id ? l['target'] : l['source']);
+    // get all links between the direct neighbours
+    var indir_links = graph_data['links'].filter(
+      l => dir_ids.includes(l['source']) && dir_ids.includes(l['target']))
+
+    var nodes = graph_data['nodes'].filter(n => dir_ids.includes(n.id) || id === n.id);
+
+    var community_filt = nodes.filter(n => n['group'][level] === root['group'][level])
+      .map(n => n.name)
+
+    var data = {
+        'nodes': nodes,
+        'links': dir_links.concat(indir_links),
+        'table': community_filt
+    };
+    return(data);
+});
+
+var LoadEgo = (function(username, level) {
+    var data = GetData(username, level),
+        scale = d3.scaleOrdinal(d3.schemeDark2);
+
+    if (!data) {
+      document.querySelector('#message').innerHTML = "Account not in our community database.";
+      return;
+    }
+    if (data['nodes'].length > 300) {
+      document.querySelector('#message').innerHTML = "Account related to " + data['nodes'].length + " users with more than 10 thousand followers, which can be previewed on the <a href="/">network visualisation.</a>";
+      return;
+    }
+
+    var width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
+        size = (width > 600) ? (width/ 2 - 10) : (width - 20);
+
+    var elem = document.querySelector('#table')
+    elem.setAttribute("style", "width:"  + size + "px");
+    elem.setAttribute("style", "height:" + size + "px");
+    var elem = document.querySelector('#table p')
+    elem.innerHTML += level + '</br></br>';
+    elem.innerHTML += data['table'].map(
+      n => '<a href="https://twitter.com/' + n.split(1) + '" target="_blank">' + n + '</a>')
+     .join('<br/>')
+    elem.setAttribute("style", "display: block");
+    console.log(elem);
+
+    elem = document.querySelector('#graph')
+    elem.setAttribute("style", "width:"  + size + "px");
+    elem.setAttribute("style", "height:" + size + "px");
+    const Graph = ForceGraph()(elem)
+        .graphData({ 'nodes': data['nodes'], 'links': data['links'] })
+        .backgroundColor("#ffffff")
+        .width(size)
+        .height(size)
+        .zoom(0.8)
+        // configure nodes
+        .nodeColor(n => scale(n.group[level]))
+        .nodeVal(n => n.value * 100000.0)
+        // configure links, particles travelling links indicate link direction
+        .linkCurvature(.2)
+        .linkDirectionalParticles(l => l.value)
+        .linkDirectionalParticleSpeed(l => l.value * 0.001)
+        .linkDirectionalParticleWidth(3)
+        // decorate the node
+        .nodeCanvasObject((n, ctx, globalScale) => {
+          const label = n.name;
+          ctx.font =  (Math.pow(2, 1 + n.value * 20) * 10).toString() + 'px monospace';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.strokeStyle = '#ffffff';
+          ctx.lineWidth = 4;
+          ctx.strokeText(label, n.x, n.y + 12);
+          ctx.fillStyle = n.color;
+          ctx.fillText(label, n.x, n.y + 12);
+        })
+        .nodeCanvasObjectMode(() => 'after')
+        // node remains in position after dragging
+        .onNodeDragEnd(node => {
+          node.fx = node.x;
+          node.fy = node.y;
+        })
+        // show panel on node click
+        .onNodeClick(n => OpenPanel(n.name, size, live))
+
+    // Spread nodes a little wider
+    Graph.d3Force("charge").strength(-900);
+    Graph.d3VelocityDecay(0.1)
+
+});
+
+var UserSearch = (function() {
+    var user = document.querySelector('input').value
+    LoadEgo(user, 1)
+});
+
+var LuckySearch = (function() {
+    var users = [ '@CarolineLucas', '@NetZeroWatch', '@GretaThunberg',
+        '@ExtinctionR', '@BBCNews' ]
+    var rand = Math.floor(Math.random() * users.length);
+    LoadEgo(users[rand], 1);
+});
+
+var Draw = (function() {
+  var svg = d3.select("#circles");
   var margin = 20,
     diameter = Math.min(1000,
         Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)) - 40;
+  svg.attr("width", diameter).attr("height", diameter)
 
   var g = svg.append("g")
     .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
@@ -34,7 +148,7 @@ Draw = (function() {
       .enter().append("text")
       .attr("class", "label")
       .style("display", d => d.parent === root  ? "inline" : "none")
-      .text(d => { return isNaN(d.data.name) ? d.data.name : "" });
+      .text(d => { return isNaN(d.data.name) ? "@" + d.data.name : "" });
 
     var node = g.selectAll("circle,text");
 
--- visualisation/communities.md
+++ visualisation/communities.md
@@ -2,12 +2,26 @@
 layout: home
 title: COP26 Communities Visualisation
 permalink: /visualisation/communities
-body_js:
-    - "https://d3js.org/d3.v4.js"
+head_js:
+    - "https://d3js.org/d3.v4.min.js"
     - "https://d3js.org/d3-scale-chromatic.v1.min.js"
+    - "https://unpkg.com/force-graph"
+body_js:
     - "/assets/js/communities.js"
+    - "/assets/data/ego_network.js"
 ---
 
 Below is a visualisation of communities discussing COP26 amongst the 500 most influential nodes. Click on each coloured circle to explore community members. Click on a white circle to zoom out.
 
-<svg id='circles' width="960" height="960"></svg>
+<svg id="circles"></svg>
+Search for a user: <input type="text" id="username" value="@username">
+<button id="search" onclick="UserSearch()" >Search</button>
+<button if="lucky"  onclick="LuckySearch()">I'm feeling lucky</button>
+<p id="message"></p>
+
+<div class="flexi flexi-2">
+  <div id="table">
+    <p>Users in the same community, hierarchy level:</p>
+  </div>
+  <div id="graph"></div>
+</div>
