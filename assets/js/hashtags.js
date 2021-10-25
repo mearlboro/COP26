37c6cc49196982ecfc7d13f42f9fedb84a09b6c3
mis <mearlboro@protonmail.com>
2021-10-25 23:01:43 +0100

    Update hashtags in two ideological categories, add toggle between them, and link hashtag to twitter search on click

--- assets/js/hashtags.js
+++ assets/js/hashtags.js
@@ -1,37 +1,58 @@
-Draw = (function() {
-  var cloud = d3.layout.cloud()
-    .size([
-      Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - 40,
-      Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 200 ])
-    .words(hashtagData)
+"use strict";
+
+var Redraw = (function(e) {
+    document.getElementById('greenCloud').style.display = e.checked ? 'none'  : 'block';
+    document.getElementById('redCloud'  ).style.display = e.checked ? 'block' : 'none';
+});
+
+var Draw = (function(colour) {
+  var data   = colour === 'red' ? hashtags_red : hashtags_green;
+  var elem   = '#' + colour + 'Cloud';
+  var width  = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - 40;
+  var height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 200;
+
+  const cloud = d3.layout.cloud()
+    .size([ width, height ])
+    .words(data)
     .font('monospace')
-    .fontSize((d) => 1.7 * Math.log2(d['value']))
+    // the two datasets have different scales
+    .fontSize((d) => colour === 'green'
+                   ? 1.7 * Math.log2(d['value'])
+                   : 30  * Math.log10(d['value']))
     .on("end", append);
 
   cloud.start();
 
   function append(words) {
-    var svg = d3.select("#cloud"),
-      width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) - 40,
-      height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 200;
+    var svg = d3.select(elem);
     svg.attr("width", width).attr("height", height);
 
     // colour scale
-    var scale = d3.scaleOrdinal(d3.schemeDark2);
+    var scale;
+    if (colour === 'red') {
+      scale = d3.scaleOrdinal(d3.schemeRdGy[9]);
+    }
+    else {
+      scale = d3.scaleOrdinal(d3.schemePuBuGn[9]);
+    }
 
     // Draw the word cloud
     svg.append("g")
     .attr("transform", "translate(" +cloud.size()[0] / 2 + "," +cloud.size()[1] / 2 + ")")
-    .selectAll("text").data(hashtagData)
+    .selectAll("text").data(words)
       .enter()
       .append("text")
-      .text((d) => d.text)
       .style("font-size", (d) => d.size + "px")
       .style("font-family", (d) => d.font)
       .style("fill", (d, i) => scale(i))
+      .style("cursor", "pointer")
       .attr("text-anchor", "middle")
-      .attr("transform", (d) => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")");
+      .attr("transform", (d) => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+      .text((d) => d.text)
+      .on("click", d => window.open('https://twitter.com/search?q=cop26%20%23' + d.text.slice(1)));
   }
 });
 
-Draw();
+Draw('green');
+Draw('red');
+
--- visualisation/hashtags.md
+++ visualisation/hashtags.md
@@ -3,16 +3,25 @@ layout: home
 title: Hashtag Visualisation
 permalink: /visualisation/hashtags
 head_js:
- - "https://d3js.org/d3.v4.js"
+ - "https://d3js.org/d3.v4.min.js"
  - "https://d3js.org/d3-scale-chromatic.v1.min.js"
  - "https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.5/d3.layout.cloud.js"
 body_js:
- - "/assets/data/hashtags.js"
+ - "/assets/data/hashtags_green.js"
+ - "/assets/data/hashtags_red.js"
  - "/assets/js/hashtags.js"
 ---
 
 Below are shown the most popular hashtags (word size log-proportional to frequency).
 
-<svg id="cloud">
+<div class="toggle">
+  <span class="green">Green group</span>
+  <label class="switch"><input type="checkbox" onclick="Redraw(this)"><span class="slider"></span></label>
+  <span class="red">Red group</span>
+</div>
+
+<svg id="redCloud" style="display: none">
+</svg>
+<svg id="greenCloud">
 </svg>
 
