4a758544bb32b67cb8746218d4013a5166d1ab03
MaxFalkenberg <mff113@ic.ac.uk>
2021-10-31 23:47:34 +0000

    Updated pull assets with correct filename

--- pull_assets.py
+++ pull_assets.py
@@ -8,11 +8,7 @@ def Pull_Assets():
     repo.git.add('assets/data/livegraph_150.js')
     repo.git.add('assets/data/livegraph_500.js')
     repo.git.add('assets/data/livegraph_1500.js')
-    repo.git.add('assets/data/trend_data.csv')
-    try:
-        repo.git.add('assets/data/trend_data_cf.csv')
-    except:
-        pass
+    repo.git.add('assets/data/trends_data.js')
 
     repo.git.commit('-m', 'Updated live files')
     origin.pull()
