1be2c18cb5054612dbc2709d4420527a23abd76e
MaxFalkenberg <mff113@ic.ac.uk>
2021-11-01 16:06:02 +0000

    Testing url open

--- assets/js/trends.js
+++ assets/js/trends.js
@@ -139,15 +139,15 @@ function LineChart(data, {
       .attr("font-family", "monospace")
       .attr("font-size", 14)
       .attr("text-anchor", "middle")
-      .attr("y", -8)
-      .on("click", (I, i) => window.open('https://twitter.com/search?q=cop26%20%23' + text(T[i]) ));
+      .attr("y", -8);
 
   function pointermoved(event) {
     const [xm, ym] = d3.pointer(event);
     const i = d3.least(I, i => Math.hypot(xScale(X[i]) - xm, yScale(Y[i]) - ym)); // closest point
     path.attr("stroke", ([z]) => Z[i] === z ? null : "#ddd").filter(([z]) => Z[i] === z).raise();
     dot.attr("transform", `translate(${xScale(X[i])},${yScale(Y[i])})`);
-    if (T) dot.select("text").text(T[i]);
+    if (T) dot.select("text").text(T[i])
+    .on("click", i => window.open('https://twitter.com/search?q=cop26%20%23' + text(T[i]) ));
     svg.property("value", O[i]).dispatch("input", {bubbles: true});
   }
 
