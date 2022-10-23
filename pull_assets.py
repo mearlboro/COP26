1018cde1a214eba666cb00b39a9f4d8a13291308
MaxFalkenberg <mff113@ic.ac.uk>
2022-10-23 13:08:06 +0100

    Updated live files

--- pull_assets.py
+++ pull_assets.py
@@ -5,13 +5,17 @@ def Pull_Assets():
     repo.remote().fetch()
     origin = repo.remote(name='origin')
 
-    repo.git.add('assets/data/livegraph_150.js')
-    repo.git.add('assets/data/livegraph_500.js')
-    repo.git.add('assets/data/livegraph_1500.js')
+    repo.git.add('assets/data/livegraph_150_cop27.js')
+    repo.git.add('assets/data/livegraph_500_cop27.js')
+    repo.git.add('assets/data/livegraph_1500_cop27.js')
     try:
         repo.git.add('assets/data/trends_data.js')
     except:
         print('Trend failed push')
+    #try:
+    #    repo.git.add('assets/data/livegraph_10000.js')
+    #except:
+    #    print('LG 10k failed')
     repo.git.commit('-m', 'Updated live files')
     origin.pull()
     origin.push()
